d1d6c24b99b0172411fe984ef3acb8f2029edafb
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-24 14:44:45 +0900

    feat: SimpleConnectionMaker 클래스 분리

--- src/main/java/com/example/tobyspring3/dao/UserDAO.java
+++ src/main/java/com/example/tobyspring3/dao/UserDAO.java
@@ -3,39 +3,20 @@ package com.example.tobyspring3.dao;
 import com.example.tobyspring3.domain.User;
 
 import java.sql.*;
-import java.util.Map;
 
-import static java.lang.Class.forName;
-import static java.lang.System.getenv;
+public abstract class UserDAO {
 
-public class UserDAO {
-
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
-        Map<String,String> env = getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(
-                dbHost, dbUser, dbPassword
-        );
-
-        return conn;
-    }
-
-
+    SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
 
     public void add() throws ClassNotFoundException, SQLException {
 
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
         PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values (?,?,?)");
-        pstmt.setString(1,"2");
-        pstmt.setString(2,"최민지");
+        pstmt.setString(1,"4");
+        pstmt.setString(2,"박지민");
         pstmt.setString(3, "@12$#%@!");
         pstmt.executeUpdate();
         pstmt.close();
-
         conn.close();
 
 
@@ -43,7 +24,7 @@ public class UserDAO {
 
     // getDataByID
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
         PreparedStatement pstmt = conn.prepareStatement("select id,name, password from users where id = ?");
         pstmt.setString(1,id);
 
@@ -66,9 +47,11 @@ public class UserDAO {
         UserDAO userDao = new NUserDAO();
         userDao.add();
 
-        User user1 = userDao.get("2");
+        User user1 = userDao.get("4");
         System.out.println(user1.getId());
         System.out.println(user1.getName());
         System.out.println(user1.getPassword());
     }
+
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }
